5293464b3c62bd5507717ea0d6a9c11e3cfab98f
bite7523aszx <82069373+bite7523aszx@users.noreply.github.com>
2021-12-23 12:11:18 +0800

    12/23

---
+++ js.js
@@ -0,0 +1,78 @@
+
+
+        function imgch1(){
+            document.getElementById('img1').src="game/card-effect-on.png"
+        }
+        function imgch2(){
+            document.getElementById('img2').src="game/electronic-on.png"
+        }
+        function imgch3(){
+            document.getElementById('img3').src="game/fish-on.png"
+        }
+        function imgch4(){
+            document.getElementById('img4').src="game/live-on.png"
+        }
+        function imgch5(){
+            document.getElementById('img5').src="game/lottory-on.png"
+        }
+        function imgch6(){
+            document.getElementById('img6').src="game/mechine-on.png"
+        }
+        function imgch7(){
+            document.getElementById('img7').src="game/sports-on.png"
+        }
+        function imgch8(){
+            document.getElementById('img8').src="game/fish_icon-on.png"
+        }
+        function imgch9(){
+            document.getElementById('img9').src="game/esports-on.png"
+
+        }
+        function iconclick1(){
+            alert("請先登入")
+        }
+        function iconclick2(){
+            alert("請先登入")
+        }
+        function iconclick3(){
+            alert("請先登入")
+        }
+        function log(){
+            document.getElementById("test1").style.display="block",
+            document.getElementById('logbtn').style.display='none',
+            document.getElementById('logout').style.display='block',
+            document.getElementById('lognoin').style.display='none',
+            document.getElementById('dt').style.display='block',
+            document.getElementById('iconclick1').src="tag_soon.png",
+            document.getElementById('iconclick2').src="tag_soon.png",
+            document.getElementById('iconclick3').src="tag_soon.png",
+            document.getElementById('img1').src="tag_soon.png",
+            document.getElementById('img2').src="tag_soon.png",
+            document.getElementById('img3').src="tag_soon.png",
+            document.getElementById('img4').src="tag_soon.png",
+            document.getElementById('img5').src="tag_soon.png",
+            document.getElementById('img6').src="tag_soon.png",
+            document.getElementById('img7').src="tag_soon.png",
+            document.getElementById('img8').src="tag_soon.png",
+            document.getElementById('img9').src="tag_soon.png";
+        }
+        function logout()
+        {
+            document.getElementById('logbtn').style.display='block',
+            document.getElementById('logout').style.display='none',
+            document.getElementById("test1").style.display="none",
+            document.getElementById('lognoin').style.display='block',
+            document.getElementById('dt').style.display='none',
+            document.getElementById('iconclick1').src="button/icon_slotgame.png",
+            document.getElementById('iconclick2').src="button/icon_r220.png",
+            document.getElementById('iconclick3').src="button/icon_15w.png",
+            document.getElementById('img1').src="game/card-effect.png",
+            document.getElementById('img2').src="game/electronic.png",
+            document.getElementById('img3').src="game/fish.png",
+            document.getElementById('img4').src="game/live.png",
+            document.getElementById('img5').src="game/lottory.png",
+            document.getElementById('img6').src="game/mechine.png",
+            document.getElementById('img7').src="game/sports.png",
+            document.getElementById('img8').src="game/fish_icon.png",
+            document.getElementById('img9').src="game/esports.png";
+        }
